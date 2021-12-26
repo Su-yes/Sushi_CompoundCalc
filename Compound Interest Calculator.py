54e4a0253be9cd41a6cccf6b23fa846810480b61
Suyash Bhattarai <bsuyash.1997@gmail.com>
2021-12-25 21:12:06 -0500

    Modified the plot to include pension and withdraw. Added latex rendering.

--- Compound Interest Calculator.py
+++ Compound Interest Calculator.py
@@ -9,11 +9,16 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from colors import get_color
 
-# matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+
+# for latex rendering
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.size'] = 12
+plt.rcParams['font.family'] = "serif"
 
 # Initialize parameters
 # 385*2 + 500 + 300
-deposit = 0
+deposit = 100
 m_contribution = 888 + 500
 
 retire_age = 65
@@ -44,6 +49,7 @@ for i in range(0,time):
         
     else:
         # year 2 onwards. Make addition at the beginning of compounding period
+        
         yr = 1 # locally year = 1 because calculating for each year
         contribution[i] = contribution[i-1] + y_contribution
         temp_A = A[i-1] + y_contribution
@@ -53,6 +59,24 @@ for i in range(0,time):
 # fig = plt.figure()  # an empty figure with no Axes
 # fig, ax = plt.subplots()  # a figure with a single Axes
 # ax.plot(t,principal,'-r', t, interest,'k')
+
+# =============================================================================
+# Calculate pension 
+
+avg_sal = 170000
+
+if retire_age >= 62:
+    factor = 0.011
+    pension = factor*avg_sal*time
+    
+else:
+    factor = 0.01
+    pension = factor*avg_sal*time
+
+print("\nYour pension after {} years at the age of {} is: ${:,.0f} per year.".format(time, retire_age, pension))
+
+# =============================================================================
+# Start Plots
 
 fig = plt.figure()
 fig, ax = plt.subplots()
@@ -71,29 +95,17 @@ ax.set_xlabel('Years')
 ax.set_ylabel('Balance')
 ax.legend(["Principal", "Interest"], loc = 2, frameon = False)
 
-# Display final value
-ax.text(1, 0.8*int(total[-1]), "Final Amount: ${:,.0f}".format(total[-1]), fontsize = 11)
+# Display final values
+ax.text(1, 0.8*int(total[-1]), "Final Amount: \${:,.0f}".format(total[-1]), fontsize = 11)
+ax.text(1, 0.74*int(total[-1]), "Pension: \${:,.0f}".format(pension, fontsize = 11))
+ax.text(1, 0.68*int(total[-1]), "4\% Withdraw: \${:,.0f}".format(0.04*total[-1], fontsize = 11))
 
 # Changes the y axis to currency. 
-fmt = '${x:,.0f}'
+fmt = '\${x:,.0f}'
 tick = mtick.StrMethodFormatter(fmt)
 ax.yaxis.set_major_formatter(tick)
 plt.tight_layout()
-# plt.savefig('Result.png', dpi=1200)
+plt.savefig('Result.png', dpi = 1200)
 
 # =============================================================================
-# Calculate pension 
 
-avg_sal = 84000
-
-if time >= 62:
-    factor = 0.011
-    pension = factor*avg_sal*time
-    
-else:
-    factor = 0.01
-    pension = factor*avg_sal*time
-
-print("\nYour pension after {} years at the age of {} is: ${:,.0f} per year.".format(time, retire_age, pension))
-
-# =============================================================================
